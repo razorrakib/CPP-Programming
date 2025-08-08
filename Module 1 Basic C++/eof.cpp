f3964cd9d9705f8821e7f7248cb22b0075952690
Rakibul Hassan <117431443+razorrakib@users.noreply.github.com>
2025-08-08 23:34:51 +0600

    Add files via upload

---
+++ Module 1 Basic C++/eof.cpp
@@ -0,0 +1,24 @@
+/* Bismillahir Rohmanir Roheem
+   In the Name of Allah (SWT) the Merciful the Compassionate
+
+   Rakibul Hassan (razorrakib)
+   BSc CSE - American International University Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+        EOF - End of file, if the number of times a input will be taken is not sepcified
+    */
+
+    int x;
+    while (cin >> x)
+    {
+        cout << x << endl;
+    }
+
+    return 0;
+}
