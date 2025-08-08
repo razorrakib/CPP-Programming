2977bc7f14addb539fec22e93cecff618260a741
Rakibul Hassan <117431443+razorrakib@users.noreply.github.com>
2025-08-08 23:35:51 +0600

    Add files via upload

---
+++ Module 1 Basic C++/setpresecion.cpp
@@ -0,0 +1,23 @@
+/* Bismillahir Rohmanir Roheem
+   In the Name of Allah (SWT) the Merciful the Compassionate
+
+   Rakibul Hassan (razorrakib)
+   BSc CSE - American International University Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+        set precision : to set the exact number of digits after a float/double
+        use #include<iomanip>
+        cout << fixed << setprecision(n) << float/double var
+    */
+
+    float x = 1.45676;
+    cout << fixed << setprecision(3) << x;
+
+    return 0;
+}
