1303f4569248942213fdfb91890cdf03f4371865
Rakibul Hassan <117431443+razorrakib@users.noreply.github.com>
2025-08-08 23:36:06 +0600

    Add files via upload

---
+++ Module 1 Basic C++/ternary_operator.cpp
@@ -0,0 +1,23 @@
+/* Bismillahir Rohmanir Roheem
+   In the Name of Allah (SWT) the Merciful the Compassionate
+
+   Rakibul Hassan (razorrakib)
+   BSc CSE - American International University Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+        Ternary Operator : To simplify a one tasked if else, not reccomanded for if elif else or more than 1 task
+        syntax : condition ? True/If_Code : False/Else_code
+
+    */
+
+    int x = 20;
+    x % 2 == 0 ? cout << "Even" : cout << "Odd";
+
+    return 0;
+}
